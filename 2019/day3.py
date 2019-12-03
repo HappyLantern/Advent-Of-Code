57e78c254f293559b4986ac3bf556879b16c7203
Kev <kevinjohansson1995@gmail.com>
2019-12-03 23:24:21 +0100

    d3

--- 2019/day3.py
+++ 2019/day3.py
@@ -1,58 +1,74 @@
-from shapely.geometry import LineString
 input = open('day3_input.txt', 'r')
 wire_path_1 = input.readline().strip().split(',')
 wire_path_2 = input.readline().strip().split(',')
+paths = [wire_path_1, wire_path_2]
 
-wire_paths = [wire_path_1, wire_path_2]
-wire1 = [0, 0]
-wire2 = [0, 0]
-wires = [wire1, wire2]
-path_hist_1 = []
-path_hist_2 = []
-path_hist = [path_hist_1, path_hist_2]
+distances = [] # Answer for part 1
+steps_to_intersection = [] # Answer for part 2
 
+lines = [[], []]
+lines_dir = [[], []]
+steps = [[], []]
 
-k = 0
-for i in range(len(wire_paths)):
-    for step in wire_paths[i]:
-        step_type = step[0]
-        step_dist= int(step[1:])
-    
-        if step_type == 'R':
-            wires[i][0] += step_dist
-        elif step_type == 'U':
-            wires[i][1] += step_dist
-        elif step_type == 'L':
-            wires[i][0] -= step_dist
+# Create all lines and the steps (size) of that line
+for i in range(2): 
+    start = [0, 0]
+
+    for move in paths[i]:
+        dir = move[0]
+        step = int(move[1:])
+        next = start.copy()
+        if dir == 'R':
+            next[0] += step
+            dir = 'H'
+        if dir == 'L':
+            next[0] -= step
+            dir = 'H'
+        if dir == 'D':
+            next[1] -= step
+            dir = 'V'
+        if dir == 'U':
+            next[1] += step
+            dir = 'V'
+
+        x1 = start[0]
+        y1 = start[1]
+        x2 = next[0]
+        y2 = next[1]
+
+        start = next.copy()
+
+        lines[i].append((x1, x2, y1, y2))
+        lines_dir[i].append(dir)
+
+        if len(steps[i]) == 0:
+            steps[i].append(abs(x2 - x1) + abs(y2 - y1))
         else:
-            wires[i][1] -= step_dist
+            steps[i].append(steps[i][-1] + abs(x2 - x1) + abs(y2 - y1))
 
-        curr_path = wires[i].copy()
-        path_hist[i].append(curr_path)
+# Look for intersections
+i = 0
+for line_1 in lines[0]:
+    j = 0
+    for line_2 in lines[1]:
+        if (lines_dir[0][i] == 'H' and lines_dir[1][j] == 'V' and
+                ((line_1[0] <= line_2[0] <= line_1[1]) or (line_1[0] >= line_2[0] >= line_1[1])) and
+                ((line_2[2] <= line_1[2] <= line_2[3]) or line_2[2] >= line_1[2] >= line_2[3])):
+                distances.append(line_2[0] + line_1[2])
+                line_1_steps = steps[0][i-1] + abs(line_1[0] - line_2[0])
+                line_2_steps = steps[1][j-1] + abs(line_2[2] - line_1[2])
+                steps_to_intersection.append(line_1_steps + line_2_steps)
+
+        if (lines_dir[0][i] == 'V' and lines_dir[1][j] == 'H' and
+                ((line_2[0] <= line_1[0] <= line_2[1]) or (line_2[0] >= line_1[0] >= line_2[1])) and
+                ((line_1[2] <= line_2[2] <= line_1[3]) or line_1[2] >= line_2[2] >= line_1[3])):
+                distances.append(line_1[0] + line_2[2])
+                line_1_steps = steps[0][i-1] + abs(line_1[2] - line_2[2])
+                line_2_steps = steps[1][j-1] + abs(line_2[0] - line_1[0])
+                steps_to_intersection.append(line_1_steps + line_2_steps)
+            
+        j += 1
+    i += 1
  
-
-distances = []
-
-for i in range(1, len(path_hist[0])):
-    for i in range(1, len(path_hist[1])):
-        x1 = path_hist[0][i-1][0]
-        y1 = path_hist[0][i-1][1]
-        x2 = path_hist[0][i][0]
-        y2 = path_hist[0][i][1]
-        
-        x3 = path_hist[1][i-1][0]
-        y3 = path_hist[1][i-1][1]
-        x4 = path_hist[1][i][0]
-        y4 = path_hist[1][i][1]
-
-        line1 = LineString([(x1, y1), (x2, y2)])
-        line2 = LineString([(x3, y3), (x4, y4)])
-        print(line1.intersection(line2))
-
-
-print(distances)
-# Two wirres connected to a central port and extend outward on a grid.
-# One wire per line of text, path of wire
-# Find intersection point closest to central port
-# Use manhattan distance
-
+print(min(distances)) # Part 1
+print(min(steps_to_intersection)) # Part 2
